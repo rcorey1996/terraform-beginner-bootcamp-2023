f69dc11690da24b28e8d1a369092cc4822a23bbd
Richard Corey <rcorey1996@gmail.com>
2023-10-02 19:31:16 +0000

    #18 Random S3 Bucket creation

--- main.tf
+++ main.tf
@@ -4,16 +4,29 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+     aws = {
+      source = "hashicorp/aws"
+      version = "5.19.0"
+     }
   }
 }
 
+provider "aws" {
+  # Configuration options
+}
 provider "random" {
   # Configuration options
 }
 
   resource "random_string" "bucket_name" {
-  length           = 16
-  special          = false
+    lower = true
+    upper = false
+    length = 32
+    special = false
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
 }
 
 output "random_bucket_name" {
