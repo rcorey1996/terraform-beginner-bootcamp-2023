bebd8b3c3cc3cc543f8e57ecd52d3be7c6975104
Richard Corey <rcorey1996@gmail.com>
2023-10-02 23:03:43 +0000

    #28 migrate local state to terraform cloud

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 terraform {
+   cloud {
+    organization = "rcorey1996"
+
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
+  
   required_providers {
     random = {
       source = "hashicorp/random"
